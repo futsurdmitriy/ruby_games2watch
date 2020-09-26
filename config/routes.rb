24cc6b5bc6142624f55b7598ab5bdda896ef3043
fuda <futsurdmitriy@gmail.com>
2020-09-26 22:24:43 +0200

    Created routes for list games and new game. Created Games Controller. Added actions (new,index).

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope path: "/admin" do
+    get "games" => "games#index"
+    get "games/new" => "games#new"
+  end
 end
