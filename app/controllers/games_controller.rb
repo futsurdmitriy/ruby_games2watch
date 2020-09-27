6d78e4b3d3344cbaf73ab3c485c075a919d78ae6
fuda <futsurdmitriy@gmail.com>
2020-09-28 00:48:23 +0200

    Implemented "partially" feature to save ser data in the input fields in case of invalid date submit when creating the game.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,19 +2,28 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
   def new
-    @newGame = Game.new
+    @new_game = Game.new(session[:new_game]) || Game.new
     @modes = Mode.all
     @platforms = Platform.all
   end
+
   def create
-    g = Game.new
-    g.name = params[:game][:name]
-    g.critics_rate = params[:game][:critics_rate]
-    g.genre = params[:game][:genre]
-    g.company = params[:game][:company]
-    g.publisher = params[:game][:publisher]
-    g.published_at = params[:game][:published_at]
+    g = Game.new({
+         name: params[:game][:name],
+         critics_rate: params[:game][:critics_rate],
+         genre: params[:game][:genre],
+         company: params[:game][:company],
+         publisher: params[:game][:publisher],
+         published_at: params[:game][:published_at]
+     })
+    # g.name = params[:game][:name]
+    # g.critics_rate = params[:game][:critics_rate]
+    # g.genre = params[:game][:genre]
+    # g.company = params[:game][:company]
+    # g.publisher = params[:game][:publisher]
+    # g.published_at = params[:game][:published_at]
 
     if params[:game][:platforms] && params[:game][:modes]
       g.modes = Mode.find(params[:game][:modes])
@@ -25,9 +34,20 @@ class GamesController < ApplicationController
       redirect_to "/admin/games"
     else
       flash[:errors] = g.errors.full_messages
-      params[:game] = g
-      redirect_to "/admin/games/new"
+      # game[:name] = params[:game][:name]
+
+      # redirect_to "/admin/games/new"
+      session[:new_game] = g
+      redirect_to controller: 'games', action: 'new'
       # render controller: 'games', action: 'new'
     end
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :genre, :modes, :platforms,
+                                 :published_at, :company, :publisher, :critics_rate)
+  end
+
 end
