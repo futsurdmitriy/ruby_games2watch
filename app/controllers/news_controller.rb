ad8aa12b0badfa7b6057a2024af062558c4f73e2
fuda <futsurdmitriy@gmail.com>
2020-10-01 00:53:28 +0200

    Added bootstrap.

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,69 @@
+class NewsController < ApplicationController
+  def index
+    @news = News.all
+  end
+
+  def new
+    @new_news = News.new
+  end
+
+  def create
+    @new_news = News.new(news_params)
+    if params[:news][:game]
+      @new_news.game = Game.find(params[:news][:game])
+    end
+     # Todo: make it via session to assign creator id
+    if session[:user][:id]
+      @new_news.user = User.find(session[:user][:id])
+    end
+    if @new_news.save
+      redirect_to "/admin/news"
+    else
+      flash[:errors] = @new_news.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    if params[:id]
+      @news = News.find(params[:id])
+    else
+      redirect_to "/admin/news"
+    end
+  end
+
+  def edit
+    if params[:id]
+      @news_to_edit = News.find(params[:id])
+    else
+      redirect_to "/admin/news"
+    end
+  end
+
+  def update
+    if params[:id]
+      @news_to_edit = News.find(params[:id])
+      news_to_edit_game = Integer(params[:news][:game])
+      if @news_to_edit.update(news_params) &&
+          @news_to_edit.update(game: Game.find(news_to_edit_game))
+        redirect_to "/admin/news"
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    if params[:id]
+      news = News.find(params[:id])
+      news.destroy
+      redirect_to '/admin/news'
+    end
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:headline, :image, :text)
+  end
+end
